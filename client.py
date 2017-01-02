4d5118bb7a1ce3cba5b25a3073377edaadadcbba
Mehehehe <piotrfalk@gmail.com>
2017-01-02 16:12:02 +0100

    v 0.0.2
    Added signal/slot system for server, to show it, after receiving message

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import cv2
@@ -19,20 +19,20 @@ win2 = QWidget()  # win2 - message sender
 img = QLineEdit(win2)
 msg = QLineEdit(win2)
 button = QPushButton(win2)
-msgPreview = QPushButton(win2)
-win3 = QWidget() #review of message: list form header[textbox + list], image [qpixelmap], message [textbox]
-add = QLabel(win3)
-send = QPushButton(win3)
-imma = QPixmap(win3)
+# msgPreview = QPushButton(win2)
+#win3 = QWidget() #review of message: list form header[textbox + list], image [qpixelmap], message [textbox]
+#add = QLabel(win3)
+#send = QPushButton(win3)
+#imma = QPixmap(win3)
 
 #Widget with textbox for autor, textbox for other recievers, QPIXELMAP with image and some textbox for message
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 1000)
 #handling preview and another sending option
-def review():
-    if img.text() != '': t_image = cv2.imread('client/' + img.text() + '.jpg');  t_x = image.shape[0]; t_y = image.shape[1]
-    win3.show()
+# def review():
+#     if img.text() != '': t_image = cv2.imread('client/' + img.text() + '.jpg');  t_x = image.shape[0]; t_y = image.shape[1]
+#     win3.show()
 
 #foo to handle recieved msg
 def rcv():
@@ -47,11 +47,14 @@ def imgToString(img):
 def msg_creator(): win2.show()
 
 def sendMsg():
-    if img.text() != '': image = cv2.imread('client/'+img.text()+'.jpg');  x = str(image.shape[0]); y = str(image.shape[1])
-    else: x = ''; y = ''
+    print('test')
+    if img.text() != '': print(img.text()); image = cv2.imread('client/'+img.text()+'.jpg');  x = str(image.shape[0]); y = str(image.shape[1])
+    else:   print('wchodze'); x = ''; y = ''
     #show(recvs,image,msg)
+    print('test2')
     buffer = (imgToString(image)+'|||'+msg.text()+'|||'+x+'|||'+y).encode()
     sock.connect(server_address)
+    print('sending')
     try:
         #sock.sendall(header)
         sock.sendall(buffer)
@@ -68,9 +71,9 @@ def initialize_msg_creator():
     msg.show()
     button.show()
     img.show()
-    msgPreview.move(50,125)
-    msgPreview.show()
-    msgPreview.clicked.connect(review)
+    # msgPreview.move(50,125)
+    # msgPreview.show()
+    # msgPreview.clicked.connect(review)
     button.clicked.connect(sendMsg)
 
 #main
--- file.py
+++ file.py
@@ -2,6 +2,36 @@ import sys
 import socket as s
 import cv2
 import numpy as np
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit
+from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
+from PyQt5.QtGui import QPixmap, QImage
+
+app = QApplication(sys.argv)
+window = QWidget()
+
+def BGR2RGB(image):
+    h,w,c=image.shape
+    for x in range(h):
+        for y in range(w):
+            image[x][y][2],image[x][y][0]=image[x][y][0],image[x][y][2]
+    return image
+
+class Emitter(QObject):
+    emitting = pyqtSignal()
+
+class ImageDisplayer(QObject):
+    def __init__(self):
+        self.sig=Emitter()
+        QObject.__init__(self)
+        self.sig.emitting.connect(self.showImg)
+        print('succeded')
+    def letsdothis(self):
+        print('letsdothis')
+        self.sig.emitting.emit()
+    @pyqtSlot()
+    def showImg(self):
+        print('showimg')
+        window.show()
 
 def stringToImg(string):
     string = string.split("_")
@@ -10,19 +40,36 @@ def stringToImg(string):
     return [[[np.float32(z) for z in y] for y in x] for x in string]
 
 def loadMessage(buffer):
+    print('1 post')
     img, message, h, w = buffer.split(b'|||')
-    h = h.decode()
-    w = w.decode()
-    print(message.decode())
+    h = int(h.decode())
+    w = int(w.decode())
     img = stringToImg(img.decode())
-    temp_image = np.zeros(shape=(int(h), int(w), 3), dtype=np.uint8)
-    for row in range(int(h)):
-        for value in range(int(w)):
+    temp_image = np.zeros(shape=(h, w, 3), dtype=np.uint8)
+    for row in range(h):
+        for value in range(w):
             for channel in range(3):
                 temp_image[row][value][channel] = img[row][value][channel]
-    cv2.imwrite('server/new.jpg', temp_image)
-
-
+    print('2 post')
+    img = BGR2RGB(temp_image)
+    pic = QPixmap(QImage(img, w, h, 3*w, QImage.Format_RGB888))
+    if int(w) > 500: pic = pic.scaledToWidth(500)
+    lbl = QLabel(window)
+    lbl.setPixmap(pic)
+    window.resize(pic.width(), 100)
+    author = QLineEdit(window)
+    author.setText('Nadawca')
+    rcvs = QLineEdit(window)
+    rcvs.setText('Odbiorca')
+    rcvs.move(window.width() - rcvs.width(), 0)
+    lbl.move(0, author.height())
+    msg = QLineEdit(window)
+    msg.setText(message.decode())
+    msg.move(0, author.height() + pic.height())
+    window.resize(pic.width(), pic.height() + (author.height() * 2))
+    win = ImageDisplayer()
+    win.letsdothis()
+    print('3 post')
 #init connection
 ip = 'localhost' # string
 port = 1000 # integer
@@ -48,7 +95,8 @@ while True:
                 #save to memory
             else: break
     finally:
+        print('transmision ended \n showing message')
         loadMessage(buf)
-        print('Image saved in //server// folder')
-        print('transmision finished')
+
         connection.close()
+        sys.exit(app.exec_())
